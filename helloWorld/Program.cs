f03f0e5c46e575d9733911d902ed0b31d4e756ef
samimeadad <sami.meadad@gmail.com>
2022-11-22 22:40:46 +0600

    First commit

---
+++ helloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            int x = Convert.ToInt32(Console.ReadLine());
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Sum = " + Convert.ToInt32(x+y));
+
+            Console.ReadKey();
+        }
+    }
+}
